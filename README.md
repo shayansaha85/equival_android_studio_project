cec853bfe8297d2745ddf88e3ae63ea69642cfcd
Shayan Saha <shayan851997@gmail.com>
2021-04-23 10:05:33 +0530

    android studio project for equival

---
+++ README.md
@@ -0,0 +1,17 @@
+# Android Studio project for converting the equival site (https://shayansaha85.github.io/equival) into a fully functional android app
+
+You can build your own android app from your website/any website with this project.
+You just have to make some minor changes.
+
+The changes you have to make are mentioned below
+
+<ol>
+    <li>Changes in <b>MainActivity.java</b> file
+        <ul>
+            <li>In line 33, inside the <b>webView.loadUrl()</b> function provide your website link</li>
+        </ul>
+    </li>
+    <li>Changing icon
+        <p>Make your icon and save it in a location in png format. Now open android studio, right click on 'app' in the project pannel, go to New>Image Assets. Change the path to the path of your logo file. And click on Next/Finish</p>
+    </li>
+</ol>
---
+++ app/src/main/java/com/example/mahbu/convertanywebsiteintoandroidapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.equival.app;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://shayansaha85.github.io/equival");
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setDomStorageEnabled(true);
+
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+
+        else{
+            super.onBackPressed();
+        }
+
+    }
+}
